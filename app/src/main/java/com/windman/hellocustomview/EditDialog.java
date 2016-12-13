0c8807d9ca9b69aa5b1acbaaaf9a193f465b8640
tony <tony.xia@vtrump.com>
2016-12-13 22:36:09 +0800

    完善编辑dialog

--- app/src/main/java/com/windman/hellocustomview/EditDialog.java
+++ app/src/main/java/com/windman/hellocustomview/EditDialog.java
@@ -7,9 +7,30 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Vtrump on 2016/12/13.
+ */
 
 public class EditDialog extends Dialog implements View.OnClickListener {
     private Context mContext;
+
+    private EditText mEditText;
+    private ImageView mCLear;
+    private TextView mCancel;
+    private TextView mOk;
+    private EditClickListener listener;
+
+    public interface EditClickListener {
+        void onOKClik(String text);
+    }
+
+    public void setOnEditClickListener(EditClickListener listener) {
+        this.listener = listener;
+    }
 
     public EditDialog(Context context) {
         super(context, R.style.MyDialogStyle);
@@ -20,15 +41,27 @@ public class EditDialog extends Dialog implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_edit);
+        initView();
         setCanceledOnTouchOutside(false);
         setWindowSize();
+    }
+
+    private void initView() {
+        mEditText = (EditText) findViewById(R.id.dia_ed_edit);
+        mCLear = (ImageView) findViewById(R.id.dia_ed_clear);
+        mCancel = (TextView) findViewById(R.id.dia_ed_cancel);
+        mOk = (TextView) findViewById(R.id.dia_ed_ok);
+
+        mCLear.setOnClickListener(this);
+        mCancel.setOnClickListener(this);
+        mOk.setOnClickListener(this);
     }
 
     private void setWindowSize() {
         Window dialogWindow = getWindow();
         WindowManager.LayoutParams lp = dialogWindow.getAttributes();
         DisplayMetrics d = mContext.getResources().getDisplayMetrics();
-        lp.width = (int) (d.widthPixels * 0.9);
+        lp.width = (int) (d.widthPixels * 0.8);
 //        lp.height = (int) (d.widthPixels * 0.8);
         dialogWindow.setAttributes(lp);
     }
@@ -37,8 +70,16 @@ public class EditDialog extends Dialog implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.dia_ed_cancel:
+                dismiss();
                 break;
+
             case R.id.dia_ed_ok:
+                listener.onOKClik(mEditText.getText().toString().trim());
+                dismiss();
+                break;
+
+            case R.id.dia_ed_clear:
+                mEditText.setText("");
                 break;
         }
     }
--- app/src/main/java/com/windman/hellocustomview/MainActivity.java
+++ app/src/main/java/com/windman/hellocustomview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.windman.hellocustomview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditDialog editDialog = new EditDialog(MainActivity.this);
+                editDialog.show();
+                editDialog.setOnEditClickListener(new EditDialog.EditClickListener() {
+                    @Override
+                    public void onOKClik(String text) {
+                        if (!TextUtils.isEmpty(text)) {
+                            textView.setText(text);
+                        }
+                    }
+                });
+            }
+        });
     }
 }
